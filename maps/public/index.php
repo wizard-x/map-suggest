a75a68a657368ec136d7116cba3d8f8c1ad5f5cc
Andrew Lee <awlee7@gmail.com>
2021-08-27 05:52:38 +0300

    enable enviroment variables

--- maps/public/index.php
+++ maps/public/index.php
@@ -2,8 +2,13 @@
 
 use App\Kernel;
 
+use Symfony\Component\Dotenv\Dotenv;
+
 require_once dirname(__DIR__).'/vendor/autoload_runtime.php';
 
 return function (array $context) {
+    if (method_exists(Dotenv::class, 'bootEnv')) {
+        (new Dotenv())->usePutenv(true)->bootEnv(dirname(__DIR__).'/.env');
+    }
     return new Kernel($context['APP_ENV'], (bool) $context['APP_DEBUG']);
 };
