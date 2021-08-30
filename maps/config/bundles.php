fbf828cd6895bd9954fae04cdd8b6bdfe99c585b
Andrew Lee <awlee7@gmail.com>
2021-08-31 02:04:18 +0300

    twig pack installed

--- maps/config/bundles.php
+++ maps/config/bundles.php
@@ -10,4 +10,6 @@ return [
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
+    Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
+    Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
 ];
