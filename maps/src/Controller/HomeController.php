faf6713b3f3c588affbc854169640b1e333e70c7
Andrew Lee <awlee7@gmail.com>
2021-08-31 02:08:49 +0300

    twig used

---
+++ maps/src/Controller/HomeController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Routing\Annotation\Route;
+
+/**
+ * @Route("/")
+*/
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function index(Request $request): Response
+    {
+        return $this->render('home/index.html.twig');
+    }
+}
