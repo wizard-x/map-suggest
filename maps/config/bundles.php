a53a6f3942dc2030383a8a35ce76af748ab34585
Andrew Lee <awlee7@gmail.com>
2021-08-31 00:43:18 +0300

    snc_redis installed

--- maps/config/bundles.php
+++ maps/config/bundles.php
@@ -9,4 +9,5 @@ return [
     Symfony\Bundle\MakerBundle\MakerBundle::class => ['dev' => true],
     JMS\SerializerBundle\JMSSerializerBundle::class => ['all' => true],
     FOS\RestBundle\FOSRestBundle::class => ['all' => true],
+    Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
 ];
