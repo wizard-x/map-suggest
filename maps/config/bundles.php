700fcbfa2156d407a81682ac6629f6651c0afdcd
Andrew Lee <awlee7@gmail.com>
2021-08-31 07:56:52 +0300

    webpack encore bundle added

--- maps/config/bundles.php
+++ maps/config/bundles.php
@@ -12,4 +12,5 @@ return [
     Snc\RedisBundle\SncRedisBundle::class => ['all' => true],
     Symfony\Bundle\TwigBundle\TwigBundle::class => ['all' => true],
     Twig\Extra\TwigExtraBundle\TwigExtraBundle::class => ['all' => true],
+    Symfony\WebpackEncoreBundle\WebpackEncoreBundle::class => ['all' => true],
 ];
